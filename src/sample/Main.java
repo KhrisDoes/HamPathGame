b7322f5e36947a22f7f7014118716e8a11427413
Christos <khrisdoes@gmail.com>
2018-12-15 23:17:30 +0000

    Initial commit with working version

---
+++ src/sample/Main.java
@@ -0,0 +1,27 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.paint.Paint;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        Scene myScene = new Scene(root, 300, 275);
+        myScene.setFill(Paint.valueOf("RED"));
+        primaryStage.setScene(myScene);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
